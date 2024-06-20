6894c53bfff5d150e469a114044222159fdc4665
spogo1312 <91920071+spogo1312@users.noreply.github.com>
2024-06-20 10:14:57 +0200

    publish and uci namespace

--- ZabkaChessEngine/UciHandler.cs
+++ ZabkaChessEngine/UciHandler.cs
@@ -2,219 +2,222 @@
 using System.Collections.Generic;
 using ZabkaChessEngine;
 
-public class UciHandler
+namespace ZabkaChessEngine 
 {
-    private Board board;
-    private MoveGenerator moveGenerator;
-    private MoveValidator moveValidator;
-    private static readonly Random random = new Random();
-    private bool isBotWhite;
-    private bool isWhiteTurn;
-    private bool manualPlayMode;
-
-    public UciHandler(bool isBotWhite)
+    public class UciHandler
     {
-        board = new Board();
-        moveGenerator = new MoveGenerator();
-        moveValidator = new MoveValidator();
-        this.isBotWhite = isBotWhite;
-        this.isWhiteTurn = true;  // White always starts in chess
-        this.manualPlayMode = false;
-    }
+        private Board board;
+        private MoveGenerator moveGenerator;
+        private MoveValidator moveValidator;
+        private static readonly Random random = new Random();
+        private bool isBotWhite;
+        private bool isWhiteTurn;
+        private bool manualPlayMode;
 
-    public void Start()
-    {
-        while (true)
+        public UciHandler(bool isBotWhite)
         {
-            string input = Console.ReadLine();
-            if (input == "uci")
+            board = new Board();
+            moveGenerator = new MoveGenerator();
+            moveValidator = new MoveValidator();
+            this.isBotWhite = isBotWhite;
+            this.isWhiteTurn = true;  // White always starts in chess
+            this.manualPlayMode = false;
+        }
+
+        public void Start()
+        {
+            while (true)
             {
-                Console.WriteLine("id name Zabka");
-                Console.WriteLine("id author JakPod");
-                Console.WriteLine("uciok");
-            }
-            else if (input == "isready")
-            {
-                Console.WriteLine("readyok");
-            }
-            else if (input == "ucinewgame")
-            {
-                board = new Board();
-                isWhiteTurn = true;
-            }
-            else if (input.StartsWith("position"))
-            {
-                if (input.Contains("startpos"))
+                string input = Console.ReadLine();
+                if (input == "uci")
+                {
+                    Console.WriteLine("id name Zabka");
+                    Console.WriteLine("id author JakPod");
+                    Console.WriteLine("uciok");
+                }
+                else if (input == "isready")
+                {
+                    Console.WriteLine("readyok");
+                }
+                else if (input == "ucinewgame")
                 {
                     board = new Board();
-                    isWhiteTurn = true;  // Reset to white's turn
+                    isWhiteTurn = true;
                 }
-                else if (input.Contains("fen"))
+                else if (input.StartsWith("position"))
                 {
-                    string fen = input.Substring(input.IndexOf("fen") + 4);
-                    board.SetPositionFromFEN(fen);
-                    // Reset the turn based on the FEN string
-                    isWhiteTurn = fen.Contains(" w ");
-                }
-                // Apply moves from position command
-                if (input.Contains("moves"))
-                {
-                    string[] parts = input.Split(' ');
-                    for (int i = 3; i < parts.Length; i++)
+                    if (input.Contains("startpos"))
                     {
-                        ApplyMove(parts[i], false);
+                        board = new Board();
+                        isWhiteTurn = true;  // Reset to white's turn
+                    }
+                    else if (input.Contains("fen"))
+                    {
+                        string fen = input.Substring(input.IndexOf("fen") + 4);
+                        board.SetPositionFromFEN(fen);
+                        // Reset the turn based on the FEN string
+                        isWhiteTurn = fen.Contains(" w ");
+                    }
+                    // Apply moves from position command
+                    if (input.Contains("moves"))
+                    {
+                        string[] parts = input.Split(' ');
+                        for (int i = 3; i < parts.Length; i++)
+                        {
+                            ApplyMove(parts[i], false);
+                        }
                     }
                 }
-            }
-            else if (input.StartsWith("go"))
-            {
-                MakeBotMove();
-            }
-            else if (input == "quit")
-            {
-                break;
-            }
-            else if (input == "playmanual")
-            {
-                manualPlayMode = true;
-                Console.WriteLine("Manual play mode activated. You can now make moves for both sides.");
-            }
-            else if (input == "playbotvsbot")
-            {
-                manualPlayMode = false;
-                PlayBotVsBot();
-            }
-            else if (input.StartsWith("move"))
-            {
-                string move = input.Substring(5);
-                if (ApplyMove(move, !manualPlayMode))
+                else if (input.StartsWith("go"))
                 {
-                    if (IsBotTurn() && !manualPlayMode)
+                    MakeBotMove();
+                }
+                else if (input == "quit")
+                {
+                    break;
+                }
+                else if (input == "playmanual")
+                {
+                    manualPlayMode = true;
+                    Console.WriteLine("Manual play mode activated. You can now make moves for both sides.");
+                }
+                else if (input == "playbotvsbot")
+                {
+                    manualPlayMode = false;
+                    PlayBotVsBot();
+                }
+                else if (input.StartsWith("move"))
+                {
+                    string move = input.Substring(5);
+                    if (ApplyMove(move, !manualPlayMode))
                     {
-                        MakeBotMove();
+                        if (IsBotTurn() && !manualPlayMode)
+                        {
+                            MakeBotMove();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Illegal move. Try again.");
                     }
                 }
-                else
+                else if (input == "display")
                 {
-                    Console.WriteLine("Illegal move. Try again.");
+                    Console.WriteLine("Current board state:");
+                    board.PrintBoard();
                 }
-            }
-            else if (input == "display")
-            {
-                Console.WriteLine("Current board state:");
-                board.PrintBoard();
             }
         }
-    }
 
-    private bool ApplyMove(string move, bool checkTurn)
-    {
-        int fromY = move[0] - 'a';
-        int fromX = 8 - (move[1] - '0');
-        int toY = move[2] - 'a';
-        int toX = 8 - (move[3] - '0');
-
-        Move userMove = new Move(fromX, fromY, toX, toY);
-
-        bool isWhiteMove = board.Squares[fromX, fromY].Color == PieceColor.White;
-        if (checkTurn && isWhiteMove != isWhiteTurn)
+        private bool ApplyMove(string move, bool checkTurn)
         {
+            int fromY = move[0] - 'a';
+            int fromX = 8 - (move[1] - '0');
+            int toY = move[2] - 'a';
+            int toX = 8 - (move[3] - '0');
+
+            Move userMove = new Move(fromX, fromY, toX, toY);
+
+            bool isWhiteMove = board.Squares[fromX, fromY].Color == PieceColor.White;
+            if (checkTurn && isWhiteMove != isWhiteTurn)
+            {
+                return false;
+            }
+
+            if (moveValidator.IsMoveLegal(board, userMove, isWhiteTurn))
+            {
+                // Handle en passant capture
+                if (board.Squares[fromX, fromY].Type == PieceType.Pawn && toX == fromX + (isWhiteTurn ? -1 : 1) && toY != fromY && board.Squares[toX, toY].Type == PieceType.Empty)
+                {
+                    board.Squares[fromX, toY] = new Piece(PieceType.Empty, PieceColor.None);
+                }
+
+                board.Squares[toX, toY] = board.Squares[fromX, fromY];
+                board.Squares[fromX, fromY] = new Piece(PieceType.Empty, PieceColor.None);
+                board.EnPassantTarget = moveValidator.enPassantTarget;
+                moveValidator.LastMove = userMove;
+
+                Console.WriteLine("Current board state:");
+                board.PrintBoard();
+
+                isWhiteTurn = !isWhiteTurn;  // Switch turn
+                return true;
+            }
             return false;
         }
 
-        if (moveValidator.IsMoveLegal(board, userMove, isWhiteTurn))
+        private bool IsBotTurn()
         {
-            // Handle en passant capture
-            if (board.Squares[fromX, fromY].Type == PieceType.Pawn && toX == fromX + (isWhiteTurn ? -1 : 1) && toY != fromY && board.Squares[toX, toY].Type == PieceType.Empty)
+            return isBotWhite == isWhiteTurn;
+        }
+
+        private void MakeBotMove()
+        {
+            List<Move> allMoves = moveGenerator.GenerateAllMoves(board, isBotWhite);
+            List<Move> legalMoves = new List<Move>();
+
+            foreach (Move move in allMoves)
             {
-                board.Squares[fromX, toY] = new Piece(PieceType.Empty, PieceColor.None);
+                if (moveValidator.IsMoveLegal(board, move, isBotWhite))
+                {
+                    legalMoves.Add(move);
+                }
             }
 
-            board.Squares[toX, toY] = board.Squares[fromX, fromY];
-            board.Squares[fromX, fromY] = new Piece(PieceType.Empty, PieceColor.None);
-            board.EnPassantTarget = moveValidator.enPassantTarget;
-            moveValidator.LastMove = userMove;
-
-            Console.WriteLine("Current board state:");
-            board.PrintBoard();
-
-            isWhiteTurn = !isWhiteTurn;  // Switch turn
-            return true;
-        }
-        return false;
-    }
-
-    private bool IsBotTurn()
-    {
-        return isBotWhite == isWhiteTurn;
-    }
-
-    private void MakeBotMove()
-    {
-        List<Move> allMoves = moveGenerator.GenerateAllMoves(board, isBotWhite);
-        List<Move> legalMoves = new List<Move>();
-
-        foreach (Move move in allMoves)
-        {
-            if (moveValidator.IsMoveLegal(board, move, isBotWhite))
+            if (legalMoves.Count > 0)
             {
-                legalMoves.Add(move);
+                Move move = legalMoves[random.Next(legalMoves.Count)];
+                string moveString = $"{(char)(move.FromY + 'a')}{8 - move.FromX}{(char)(move.ToY + 'a')}{8 - move.ToX}";
+                Console.WriteLine($"bestmove {moveString}");
+
+                ApplyMove(moveString, false);
+            }
+            else
+            {
+                Console.WriteLine("bestmove (none)");
             }
         }
-
-        if (legalMoves.Count > 0)
+        private void PlayBotVsBot()
         {
-            Move move = legalMoves[random.Next(legalMoves.Count)];
-            string moveString = $"{(char)(move.FromY + 'a')}{8 - move.FromX}{(char)(move.ToY + 'a')}{8 - move.ToX}";
-            Console.WriteLine($"bestmove {moveString}");
-
-            ApplyMove(moveString, false);
-        }
-        else
-        {
-            Console.WriteLine("bestmove (none)");
-        }
-    }
-    private void PlayBotVsBot()
-    {
-        while (true)
-        {
-            if (MakeBotMoveWithCheck())
+            while (true)
             {
-                break;
-            }
-        }
-    }
-
-    private bool MakeBotMoveWithCheck()
-    {
-        List<Move> allMoves = moveGenerator.GenerateAllMoves(board, isWhiteTurn);
-        List<Move> legalMoves = new List<Move>();
-
-        foreach (Move move in allMoves)
-        {
-            if (moveValidator.IsMoveLegal(board, move, isWhiteTurn))
-            {
-                legalMoves.Add(move);
+                if (MakeBotMoveWithCheck())
+                {
+                    break;
+                }
             }
         }
 
-        if (legalMoves.Count > 0)
+        private bool MakeBotMoveWithCheck()
         {
-            Move move = legalMoves[random.Next(legalMoves.Count)];
-            string moveString = $"{(char)(move.FromY + 'a')}{8 - move.FromX}{(char)(move.ToY + 'a')}{8 - move.ToX}";
-            Console.WriteLine($"bestmove {moveString}");
+            List<Move> allMoves = moveGenerator.GenerateAllMoves(board, isWhiteTurn);
+            List<Move> legalMoves = new List<Move>();
 
-            ApplyMove(moveString, false);
-            isWhiteTurn = !isWhiteTurn;  // Switch turn
+            foreach (Move move in allMoves)
+            {
+                if (moveValidator.IsMoveLegal(board, move, isWhiteTurn))
+                {
+                    legalMoves.Add(move);
+                }
+            }
 
-            return false;  // Continue game
-        }
-        else
-        {
-            Console.WriteLine(isWhiteTurn ? "Black wins" : "White wins");
-            return true;  // End game
+            if (legalMoves.Count > 0)
+            {
+                Move move = legalMoves[random.Next(legalMoves.Count)];
+                string moveString = $"{(char)(move.FromY + 'a')}{8 - move.FromX}{(char)(move.ToY + 'a')}{8 - move.ToX}";
+                Console.WriteLine($"bestmove {moveString}");
+
+                ApplyMove(moveString, false);
+                isWhiteTurn = !isWhiteTurn;  // Switch turn
+
+                return false;  // Continue game
+            }
+            else
+            {
+                Console.WriteLine(isWhiteTurn ? "Black wins" : "White wins");
+                return true;  // End game
+            }
         }
     }
 }
-}
+
